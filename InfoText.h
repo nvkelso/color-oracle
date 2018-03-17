0c2b5c00cdc40b8196654728af24aa5936851ba1
Bernie Jenny <bernie.jenny@monash.edu>
2018-03-17 20:09:40 +1100

    Changed version to 1.1.5

--- InfoText.h
+++ InfoText.h
@@ -13,7 +13,7 @@
 #define DEUTANINFOTEXT @"Common\nGreen deficiency affects about 5% of all males."
 #define PROTANINFOTEXT @"Rare\nRed deficiency affects about 2.5% of all males."
 #define TRITANINFOTEXT @"Extremely rare\nBlue deficiency affects about 0.5% of all males."
-#define GRAYSCINFOTEXT @"Luminance preserving grayscale simulation.\nY=0.2126*R + 0.7152*G + 0.0722*B"
+#define GRAYSCINFOTEXT @"Luminance preserving grayscale simulation.\nY=0.2126×R + 0.7152×G + 0.0722×B"
 
 #define NORMALTEXT @"Normal View"
 #define TOOLTIPTEXT @"Color Oracle - Simulate Color Blind Vision"
